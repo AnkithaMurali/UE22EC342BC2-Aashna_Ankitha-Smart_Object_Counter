0f0b77559498c9ac8f1a0d2a352dfeedb77551f3
Aa091 <162326436+Aa091@users.noreply.github.com>
2025-05-16 13:55:36 +0530

    object_counter_node (1).py

--- object_counter_node (1).py
+++ object_counter_node (1).py
@@ -7,9 +7,9 @@ def listener():
     node = rclpy.create_node('object_count_listener')
     publisher = node.create_publisher(Int32, 'object_count', 10)
 
-    # Open the serial port where Arduino is connected (replace with your serial port)
-    serial_port = '/dev/ttyUSB0'  # Replace with your Arduino's serial port
-    baud_rate = 9600  # Same baud rate as in Arduino code
+    # Open the serial port where Arduino is connected 
+    serial_port = '/dev/ttyUSB0'  
+    baud_rate = 9600  
     ser = serial.Serial(serial_port, baud_rate)
 
     while rclpy.ok():
